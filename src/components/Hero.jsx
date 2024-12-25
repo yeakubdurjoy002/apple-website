4cd31f4c20fbb09c68e2856d7aa916f24bb25590
YeakubDurjoy001 <yeakubdurjoy002>
2024-12-25 23:16:57 +0600

    finished the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,6 +23,7 @@ const Hero = () => {
   }, []);
   useGSAP(() => {
     gsap.to("#hero", { opacity: 1, delay: 1.5 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2.5 });
   }, []);
   return (
     <section className="w-full nav-height bg-black relative">
@@ -41,6 +42,15 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div
+        id="cta"
+        className=" flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#Highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From 199$ month or 999$</p>
       </div>
     </section>
   );
